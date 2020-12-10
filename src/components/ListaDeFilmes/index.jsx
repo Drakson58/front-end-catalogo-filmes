d409d62b37127d018929209e70bb62b2409332e3
Drakson <darksonbezerra123@gmail.com>
2020-12-09 21:05:24 -0300

    Paginação adicionada

--- src/components/ListaDeFilmes/index.jsx
+++ src/components/ListaDeFilmes/index.jsx
@@ -7,18 +7,26 @@ import './styles.css'
 
 const ListarFilmes = () => {
 
+    const [pagina, setPagina] = useState(1)
     const [filmes, setFilmes] = useState([])
     const [textoFiltrado, setTextoFiltrado] = useState("")
 
+    function ordenaFilmes (filmes) {
+        filmes.sort(function (a, b) {
+            return (a.title > b.title) ? 1 : ((b.nome > a.title) ? -1 : 0);    
+        });
+    }
+
     useEffect(() => {
         const load = async () => {
-            const response = await axiosApi.get('filmes-populares')
+            const response = await axiosApi.get(`filmes-populares/${pagina}`)
+            var filmes =  ordenaFilmes(response.data)
             setFilmes(response.data)
         }
         load()
-    }, [])
+    }, [pagina])
 
-
+    
     const buscarFilme = (event) => {
         setTextoFiltrado(event.target.value);
     }
@@ -34,7 +42,32 @@ const ListarFilmes = () => {
                     placeholder="Digite o nome do filme..."
                     value={textoFiltrado}
                 />
+                <div className="ml-3">
+                    <nav>
+                        <ul class="pagination pagination-lg">
+                            <li class="page-item">
+                                <a class="page-link" onClick={ () => setPagina(1)}>1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" onClick={ () => setPagina(2)}>2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" onClick={ () => setPagina(3)}>3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" onClick={ () => setPagina(4)}>4</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" onClick={ () => setPagina(5)}>5</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" onClick={ () => setPagina(6)}>6</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
+
             <Row>
                 {filmes?.map(filme => {
                     return (filme.title.toUpperCase().includes(textoFiltrado.toUpperCase())) ?  (
